60efab5dfb5eebb0b316fe5137287bc8634a12e8
aFku <zapasowyrowerek@gmail.com>
2025-08-30 15:30:30 +0200

    Project membership

--- services/ProjectManagementService/project_management_service/projects_app/models.py
+++ services/ProjectManagementService/project_management_service/projects_app/models.py
@@ -8,5 +8,40 @@ class Project(models.Model):
 
     # sprints - Sprint model. 1 Project have many sprints. 1 Sprint have 1 Project
     # tasks - Task model. 1 Project have many tasks. 1 Task have 1 project
-    # users_with_access - User profile model. Many users have access to many projects
+    # members - User profile model. Many users have access to many projects
     # chat_observers - User profile model. Many users have observes many chats
+
+    def get_members(self, role=None):
+        if role:
+            return ProjectMember.objects.filter(project=self, role=role)
+        else:
+            return ProjectMember.objects.filter(project=self)
+
+    def role_by_user_id(self, user_id: str):
+        try:
+            return ProjectMember.objects.get(project=self, user_id=user_id).role
+        except models.ObjectDoesNotExist:
+            return None
+
+    def add_member(self, user_id: str, role):
+        return ProjectMember.objects.get_or_create(project=self, user_id=user_id, role=role)
+
+    def remove_member(self, user_id: str):
+        ProjectMember.objects.filter(project=self, user_id=user_id).delete()
+
+class ProjectMember(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.CharField() # supplied by request, userID not stored in this project
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='members')
+
+    class Role(models.TextChoices):
+        VIEWER = 'Viewer'
+        DEVELOPER = 'Developer'
+        ADMIN = 'Admin'
+
+    role = models.CharField(choices=Role.choices,
+                              default=Role.DEVELOPER,
+                              blank=False)
+
+    def get_role(self) -> Role:
+        return self.Role(self.role)
