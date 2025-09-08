a80adbfae64f2f3e7d249ad4d7c0acc231e77960
aFku <zapasowyrowerek@gmail.com>
2025-09-08 22:27:29 +0200

    Simple projects endpoint + simple serializer

--- services/ProjectManagementService/project_management_service/projects_app/views.py
+++ services/ProjectManagementService/project_management_service/projects_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from .serializers import ProjectSerializer
+from .models import Project
+
+@csrf_exempt
+def projects(request):
+    if request.method == 'GET':
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
