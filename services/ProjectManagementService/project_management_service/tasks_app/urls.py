6faf73c32c3a464ce16e471175ffd4cc64b8753c
aFku <zapasowyrowerek@gmail.com>
2025-09-19 19:11:32 +0200

    URL typo fix

--- services/ProjectManagementService/project_management_service/tasks_app/urls.py
+++ services/ProjectManagementService/project_management_service/tasks_app/urls.py
@@ -5,7 +5,7 @@ from .views import tasks_view, tasks_by_id_view, comments_by_task, comment_by_id
 urlpatterns = [
     path('', tasks_view),
     path('<str:task_pk>/', tasks_by_id_view),
-    path('<str:task_pk/comments/', comments_by_task),
+    path('<str:task_pk>/comments/', comments_by_task),
     path('<str:task_pk>/observers/', task_observers),
     path('comments/<int:comment_pk>/', comment_by_id),
 ]
