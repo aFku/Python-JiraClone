343294175d40744bf9e1497678962ac6747d7edd
aFku <zapasowyrowerek@gmail.com>
2025-10-07 21:01:57 +0200

    Class-Based view for tasks

--- services/ProjectManagementService/project_management_service/tasks_app/urls.py
+++ services/ProjectManagementService/project_management_service/tasks_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
-from .views import tasks_view, tasks_by_id_view, comments_by_task, comment_by_id, task_observers
+from .views import TasksView, TaskByIdView, CommentByIdView, CommentListCreateView, TaskObserversView
 
 urlpatterns = [
-    path('', tasks_view),
-    path('<str:task_pk>/', tasks_by_id_view),
-    path('<str:task_pk>/comments/', comments_by_task),
-    path('<str:task_pk>/observers/', task_observers),
-    path('comments/<int:comment_pk>/', comment_by_id),
+    path('', TasksView.as_view()),
+    path('<str:task_pk>/', TaskByIdView.as_view()),
+    path('<str:task_pk>/comments/', CommentListCreateView.as_view()),
+    path('<str:task_pk>/observers/', TaskObserversView.as_view()),
+    path('comments/<int:comment_pk>/', CommentByIdView.as_view()),
 ]
--- services/ProjectManagementService/project_management_service/tasks_app/views.py
+++ services/ProjectManagementService/project_management_service/tasks_app/views.py
@@ -1,10 +1,12 @@
 import uuid
 
-from rest_framework.views import csrf_exempt
-from rest_framework.decorators import api_view
-from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import csrf_exempt, APIView
 from rest_framework import status
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Task, Comment, TaskObserver
 from .serializers import (TaskSerializer, TaskCreateSerializer, TaskUpdateSerializer, CommentSerializer,
@@ -12,127 +14,171 @@ from .serializers import (TaskSerializer, TaskCreateSerializer, TaskUpdateSerial
                           TaskObserverSerializer)
 from .filters import TaskFilter, CommentFilter
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def tasks_view(request):
-    if request.method == 'GET':
-        tasks = Task.objects.all()
-        filterset = TaskFilter(request.GET, queryset=tasks)
-        if filterset.is_valid():
-            tasks = filterset.qs
-        serializer = TaskSerializer(tasks, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TaskCreateSerializer(data=data, context={'user_id': uuid.uuid4()}) #  replace with real user id later
-        if serializer.is_valid():
-            obj = serializer.save()
-            read_serializer = TaskSerializer(obj)
-            return JsonResponse(read_serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TasksView(generics.ListCreateAPIView):
+    """
+    Class for List / Create Tasks
+    GET - Fetch list of accessible tasks
+    POST - Create Task
+    """
+
+    queryset = Task.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TaskFilter
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return TaskSerializer
+        if self.request.method == "POST":
+            return TaskCreateSerializer
+        return super().get_serializer_class()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(
+            data=request.data,
+            context={'user_id': uuid.uuid4()}
+        )  #  TO DO: replace with real user id later
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        response_details = TaskSerializer(instance=serializer.instance)
+        headers = self.get_success_headers(serializer.data)
+        return Response(response_details.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class TaskByIdView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View for managing single task
+    GET - Get task details
+    PATCH - Update partially task
+    DELETE - Remove task
+    """
+
+
+    queryset = Task.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'task_pk'
+    http_method_names = ['get', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.request.method in ['GET', 'DELETE']:
+            return TaskSerializer
+        if self.request.method == 'PATCH':
+            return TaskUpdateSerializer
+        return super().get_serializer_class()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        response_details = TaskSerializer(instance=serializer.instance)
+        return Response(response_details.data, status=status.HTTP_200_OK)
 
 
 
-@csrf_exempt
-@api_view(['GET', 'PATCH', 'DELETE'])
-def tasks_by_id_view(request, task_pk):
-    try:
-        task = Task.objects.get(id=task_pk)
-    except Task.DoesNotExist:
-        return JsonResponse({"errors": f'Task {task_pk} does not exists'}, status=status.HTTP_404_NOT_FOUND)
+class CommentListCreateView(generics.ListCreateAPIView):
+    """
+    View for List / Create comments related to specific task
+    GET - Get all comments for given task
+    POST - Create new comment for given task
+    """
 
-    if request.method == 'GET':
-        serializer = TaskSerializer(task)
-        return JsonResponse(serializer.data)
-    elif request.method == 'PATCH':
-        data = JSONParser().parse(request)
-        serializer = TaskUpdateSerializer(instance=task, data=data, partial=True)
-        if serializer.is_valid():
-            obj = serializer.save()
-            read_serializer = TaskSerializer(obj)
-            return JsonResponse(read_serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        task.delete()
-        return JsonResponse({}, status=status.HTTP_204_NO_CONTENT)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CommentFilter
 
-
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def comments_by_task(request, task_pk):
-    try:
-        task = Task.objects.get(id=task_pk)
-    except Task.DoesNotExist:
-        return JsonResponse({"errors": f'Task {task_pk} does not exists'}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
+    def get_queryset(self):
+        task_pk = self.kwargs["task_pk"]
+        task = get_object_or_404(Task, pk=task_pk)
         comments = Comment.objects.filter(task=task)
-        filterset = CommentFilter(request.GET, queryset=comments)
-        if filterset.is_valid():
-            comments = filterset.qs
-        serializer = CommentSerializer(comments, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CommentCreateSerializer(data=data, context={"user_id": uuid.uuid4(), "task": task})
-        if serializer.is_valid():
-            obj = serializer.save()
-            read_serializer = CommentSerializer(obj)
-            return JsonResponse(read_serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return comments
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return CommentSerializer
+        if self.request.method == "POST":
+            return CommentCreateSerializer
+        return super().get_serializer_class()
+
+    def perform_create(self, serializer):
+        task_pk = self.kwargs["task_pk"]
+        task = get_object_or_404(Task, pk=task_pk)
+        serializer.save(
+            task=task,
+            user_id=uuid.uuid4() #  TO DO: replace with real user id later
+        )
 
 
-@csrf_exempt
-@api_view(['GET', 'PATCH', 'DELETE'])
-def comment_by_id(request, comment_pk):
-    try:
-        comment = Comment.objects.get(id=comment_pk)
-    except Comment.DoesNotExist:
-        return JsonResponse({"errors": f'Comment {comment_pk} does not exists'}, status=status.HTTP_404_NOT_FOUND)
+class CommentByIdView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View for managing existing comment
+    GET - Get comment details
+    PATCH - Edit comment
+    DELETE - Remove comment
+    """
 
-    if request.method == 'GET':
-        serializer = CommentSerializer(comment)
-        return JsonResponse(serializer.data)
+    queryset = Comment.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'comment_pk'
+    http_method_names = ['get', 'patch', 'delete']
 
-    elif request.method == 'PATCH':
-        data = JSONParser().parse(request)
-        serializer = CommentUpdateSerializer(instance=comment, data=data, partial=True)
-        if serializer.is_valid():
-            obj = serializer.save()
-            read_serializer = CommentSerializer(obj)
-            return JsonResponse(read_serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_serializer_class(self):
+        if self.request.method in ['GET', 'DELETE']:
+            return CommentSerializer
+        if self.request.method == 'PATCH':
+            return CommentUpdateSerializer
+        return super().get_serializer_class()
 
-    elif request.method == 'DELETE':
-        comment.delete()
-        return JsonResponse({}, status=status.HTTP_204_NO_CONTENT)
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        response_details = CommentSerializer(instance=serializer.instance)
+        return Response(response_details.data, status=HTTP_200_OK)
 
 
-@csrf_exempt
-@api_view(['GET', 'POST', 'DELETE'])
-def task_observers(request, task_pk):
-    try:
-        task = Task.objects.get(id=task_pk)
-    except Task.DoesNotExist:
-        return JsonResponse({"errors": f'Task {task_pk} does not exists'}, status=status.HTTP_404_NOT_FOUND)
+class TaskObserversView(APIView):
+    """
+    View for managing observers inside task
+    GET - List all observers in task
+    POST - Add observer to task (Current user only)
+    DELETE - Remove observer from task (Current user only)
+    """
 
-    if request.method == 'GET':
-        observers = task.observers
-        serializer = TaskObserverSerializer(observers, many=True)
-        return JsonResponse(serializer.data, safe=False)
+    def get_task(self, task_pk):
+        return get_object_or_404(Task, id=task_pk)
 
-    elif request.method == 'POST':
-        serializer = TaskObserverSerializer(data={}, context={"user_id": uuid.uuid4(), "task": task})
+    def get(self, request, task_pk):
+        task = self.get_task(task_pk)
+        task_observers = task.observers.all()
+        serializer = TaskObserverSerializer(task_observers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, task_pk):
+        task = self.get_task(task_pk)
+        serializer = TaskObserverSerializer(
+            data={},
+            context={
+                "task": task,
+                "user_id": uuid.uuid4() #  TO DO: replace with real user id later
+            }
+        )
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED, safe=False)
-        else:
-            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        user_id = "PLACEHOLDER" # TO DO
+    def delete(self, request, task_pk):
+        task = self.get_task(task_pk)
+        user_id = request.query_params.get('user_id') #  TO DO: replace with current user id later
+        if not user_id:
+            return Response({"error": "Missing user_id parameter"}, status=status.HTTP_400_BAD_REQUEST) # Remove after TO DO
         deleted, _ = TaskObserver.objects.filter(task=task, user_id=user_id).delete()
         if deleted:
-            return JsonResponse({}, status=status.HTTP_204_NO_CONTENT)
-        else:
-            return JsonResponse({"error": "Observer not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"error": "Observer not found"}, status=status.HTTP_404_NOT_FOUND)
