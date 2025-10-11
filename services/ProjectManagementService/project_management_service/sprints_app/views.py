97ca39fd67bdd45f96d69345bd80c46759264997
aFku <zapasowyrowerek@gmail.com>
2025-10-11 12:46:02 +0200

    Permissions for sprints

--- services/ProjectManagementService/project_management_service/sprints_app/views.py
+++ services/ProjectManagementService/project_management_service/sprints_app/views.py
@@ -1,23 +1,34 @@
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import permissions
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Sprint
 from .serializers import SprintsSerializer, SprintCreateSerializer, SprintUpdateSerializer
 from .filters import SprintFilter
+from ..projects_app.permissions import IsViewerOrDeny, IsDeveloperOrDeny, IsAdminOrDeny
 
 
 class SprintsView(generics.ListCreateAPIView):
     """
     View for managing Sprints (Create/Fetch All)
-    List - handled by default
-    Create - Custom implementation (Changing serializer for response)
+    List - handled by default (by viewers)
+    Create - Custom implementation (Changing serializer for response) (for Devs and Admins)
     """
 
     queryset = Sprint.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_class = SprintFilter
+    methods_permissions_classes = {
+        'GET': [IsViewerOrDeny],
+        'POST': [IsDeveloperOrDeny]
+    }
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated]
+        permission_classes += self.methods_permission_classes.get(self.request.method, [])
+        return [p() for p in permission_classes]
 
     def get_serializer_class(self):
         if self.request.method == "GET":
@@ -39,15 +50,25 @@ class SprintsView(generics.ListCreateAPIView):
 class SprintByIdView(generics.RetrieveUpdateDestroyAPIView):
     """
     Class for interacting with one specific sprint
-    Get - handled by default
-    Delete - handled by default
-    Patch - custom implementation. Using different serializers for update and response
+    Get - handled by default (for viewers)
+    Delete - handled by default (for admins)
+    Patch - custom implementation. Using different serializers for update and response (for devs and admins)
     """
 
     queryset = Sprint.objects.all()
     lookup_field = 'id'
     lookup_url_kwarg = 'sprint_pk'
     http_method_names = ['get', 'patch', 'delete']
+    methods_permissions_classes = {
+        'GET': [IsViewerOrDeny],
+        'PATCH': [IsDeveloperOrDeny],
+        'DELETE': [IsAdminOrDeny]
+    }
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated]
+        permission_classes += self.methods_permission_classes.get(self.request.method, [])
+        return [p() for p in permission_classes]
 
     def get_serializer_class(self):
         if self.request.method in ["GET", "DELETE"]:
